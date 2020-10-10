c8aaa90f0a67d1122c75cc4726327b3832791065
Xiaoning Zhao <TUG64330@TEMPLE.EDU>
2020-10-09 20:24:17 -0400

    modified:   mmult.c

--- mmult.c
+++ mmult.c
@@ -23,12 +23,15 @@
 int mmult(double *c, 
 	      double *a, int aRows, int aCols, 
 	      double *b, int bRows, int bCols) {
-
+    
     for(int i = 0; i < aRows; ++i) {
         for(int j = 0; j < bCols; ++j) {
             c[i * bCols + j] = 0;
             for(int k = 0; k < aRows; ++k) {
                 c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
+                c[i * bCols + j] += a[i * aRows + k+1] * b[k * bCols + j];
+                c[i * bCols + j] += a[i * aRows + k+2] * b[k * bCols + j];
+                c[i * bCols + j] += a[i * aRows + k+3] * b[k * bCols + j];
             }
         }
     }
