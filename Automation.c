952246345baf943d04ab37dd1b7f3abfb11360ba
jimzxn <59928571+jimzxn@users.noreply.github.com>
2020-10-15 13:34:39 -0400

    bug fix

--- Automation.c
+++ Automation.c
@@ -6,35 +6,33 @@
 
 #include "mat.h"
 
-
-
-int main(int argc, char const *argv[]){
-    
-    for (size_t i = 100; i < 1000; i+=100)
-    { 
+int main(int argc, char const *argv[])
+{
+    double *a, *b, *c, time_spend;
+    clock_t begin, end;
+    for (size_t i = 100; i < 1000; i += 100)
+    {
         //nonoptimized
-        clock_t begin =clock();
-        double *a = malloc(i*i*8);
-        double *b = malloc(i*i*8);
-        a = gen_matrix(i,i);
-        b = gen_matrix(i,i);
-        double *c = malloc(i*i*8);
-        mmult(c,a,i,i,b,i,i);
-        clock_t end =clock();
-        double time_spend =((double) (end - begin)) / CLOCKS_PER_SEC; 
-        printf("Time use:%f\n",time_spend);
+        begin = clock();
+        a = malloc(i * i * 8);
+        b = malloc(i * i * 8);
+        a = gen_matrix(i, i);
+        b = gen_matrix(i, i);
+        c = malloc(i * i * 8);
+        mmult(c, a, i, i, b, i, i);
+        end = clock();
+        time_spend = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        printf("Time use:%f\n", time_spend);
         //simd
-        clock_t begin =clock();
-        double *a = malloc(i*i*8);
-        double *b = malloc(i*i*8);
-        a = gen_matrix(i,i);
-        b = gen_matrix(i,i);
-        double *c = malloc(i*i*8);
-        mmult_mmult_vectorized(c,a,i,i,b,i,i);
-        clock_t end =clock();
-        double time_spend =((double) (end - begin)) / CLOCKS_PER_SEC; 
-        printf("Time use for simd:%f\n",time_spend);
-        
+        begin = clock();
+        a = malloc(i * i * 8);
+        b = malloc(i * i * 8);
+        a = gen_matrix(i, i);
+        b = gen_matrix(i, i);
+        c = malloc(i * i * 8);
+        mmult_mmult_vectorized(c, a, i, i, b, i, i);
+        end = clock();
+        time_spend = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        printf("Time use for simd:%f\n", time_spend);
     }
-    
 }
