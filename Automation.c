dc47603c1c9b388c288da50d103fb285b90a79e5
jimzxn <59928571+jimzxn@users.noreply.github.com>
2020-10-15 13:13:29 -0400

    bug fix

--- Automation.c
+++ Automation.c
@@ -15,10 +15,10 @@ int main(int argc, char const *argv[]){
         clock_t begin =clock();
         int *a = malloc(i*8);
         int *b = malloc(i*8);
-        a = gen_matrix(i,i);
-        b = gen_matrix(i,i);
+        *a = gen_matrix(i,i);
+        *b = gen_matrix(i,i);
         int *c = malloc(i*8);
-        mmult(c,a,i,i,b,i,i);
+        mmult(*c,*a,i,i,*b,i,i);
         clock_t end =clock();
         double time_spend =((double) (end - begin)) / CLOCKS_PER_SEC; 
         printf("Time use:%f",time_spend);
