303769dc9cba5acd0137b195490ce5e6b92c37b5
jimzxn <59928571+jimzxn@users.noreply.github.com>
2020-10-16 13:23:51 -0400

    omp first

--- Automation.c
+++ Automation.c
@@ -33,6 +33,17 @@ int main(int argc, char const *argv[])
         mmult_vectorized(c, a, i, i, b, i, i);
         end = clock();
         time_spend = ((double)(end - begin)) / CLOCKS_PER_SEC;
+        printf("%10f, ", time_spend);
+        //OMP
+        begin = clock();
+        a = malloc(i * i * 8);
+        b = malloc(i * i * 8);
+        a = gen_matrix(i, i);
+        b = gen_matrix(i, i);
+        c = malloc(i * i * 8);
+        mmult_omp(c, a, i, i, b, i, i);
+        end = clock();
+        time_spend = ((double)(end - begin)) / CLOCKS_PER_SEC;
         printf("%10f\n", time_spend);
     }
 }
